14efd9be007683a3f6a2e2258efa38bd3fee8e59
ahmedalnagdy18 <ahmedalnagdy188@gmail.com>
2024-05-28 11:16:49 +0300

    some fix

--- lib/features/home/data/data_source/data_source.dart
+++ lib/features/home/data/data_source/data_source.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_app/features/home/data/model/api_model.dart';
 
 class FirebaseDataSource {
   Stream<QuerySnapshot> getPostsData() {
@@ -11,5 +14,20 @@ class FirebaseDataSource {
     CollectionReference story = FirebaseFirestore.instance.collection("Story");
     Stream<QuerySnapshot> taskstype = story.snapshots();
     return taskstype;
+  }
+
+  Future<Stream<QuerySnapshot<Object?>>> fetchPosts() async {
+    final response = await http
+        .get(Uri.parse('https://dummyjson.com/posts?skip=$skip&limit=$limit'));
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      Future<Stream<QuerySnapshot<Object?>>> posts = [];
+      for (var element in data['posts']) {
+        posts.add(ApiModel.fromJson(element));
+      }
+      return posts;
+    } else {
+      throw Exception('Failed to load posts');
+    }
   }
 }
